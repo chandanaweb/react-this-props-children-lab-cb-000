3dd3614a2a5a763d8206b3945580096c4cb4471b
chandana M <chandu11288@gmail.com>
2018-06-25 14:52:07 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,10 @@
-// Code Invitation Component Here
+import React from 'react';
+import ReactDOm from 'react-dom';
+
+class Invitation extends React.Component{
+  render(){
+    return(
+      <h1></h1>
+    )
+  }
+}
